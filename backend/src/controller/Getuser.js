ef297a9e16023762db27791eb96742dfd0a99775
Harishvow <harisharish982005@gmail.com>
2026-05-29 22:27:51 +0530

    fix:Define return output with Name

--- backend/src/controller/Getuser.js
+++ backend/src/controller/Getuser.js
@@ -4,5 +4,5 @@ exports.getUser=async(req,res)=>{
      const userkey=req.body.Userapikey
      const Message=req.body.message
     const llmapikey=await RetriveId.RetriveId(userkey)
-    res.send({llmapikey,Message})
+    res.send({"apikey":llmapikey,"Message":Message})
 }
