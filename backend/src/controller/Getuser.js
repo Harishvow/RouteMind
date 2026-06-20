22373b56f515e604eb446de0142aa28350165cd5
Harishvow <harisharish982005@gmail.com>
2026-06-20 18:05:10 +0530

    feat:Implement rate limiting for getApiKey endpoint and add express-rate-limit dependency

--- backend/src/controller/Getuser.js
+++ backend/src/controller/Getuser.js
@@ -3,6 +3,6 @@ const RetriveId=require("../service/Retriveparent")
 exports.getUser=async(req,res)=>{
      const userkey=req.body.Userapikey
      const Message=req.body.message
-    const llmapikey=await RetriveId.RetriveId(userkey)
+     const llmapikey=await RetriveId.RetriveId(userkey)
     res.send({"apikey":llmapikey,"Message":Message})
 }
--- backend/src/routes/Projectdetails.js
+++ backend/src/routes/Projectdetails.js
@@ -2,14 +2,19 @@ const express=require("express");
 const {SaveUserApikey} = require("../controller/Userapikey");
 const {Saveprojectdetails} = require("../controller/Detailsproject");
 const {getUser} = require("../controller/Getuser")
+const rateLimiter=require("express-rate-limit")
 
-
+const limiter=rateLimiter({
+    windowMs:60*1000,
+    max:10,
+    message:"Too many requests limit exceeded,please try agian after minute"
+})
 
 const router=express.Router();
 
 
 router.post("/UserApiKey",SaveUserApikey)
 router.post("/ProjectDetails",Saveprojectdetails,)
-router.get("/getApiKey",getUser)
+router.post("/getApiKey",limiter,getUser)
 
 module.exports = router;
